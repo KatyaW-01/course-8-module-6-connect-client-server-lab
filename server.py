89c9972e7b6b04437609a8426d50b748567395c5
Katya Weicht <katya.weicht@gmail.com>
2025-07-17 12:23:53 -0600

    Create home route and GET request for all events

--- server.py
+++ server.py
@@ -4,12 +4,22 @@ app = Flask(__name__)
 
 # Create a list called 'events' with a couple of sample event dictionaries
 # Each dictionary should have an 'id' and a 'title'
+events = [
+    {"id": 1, "title": "Item added to cart" },
+    {"id": 2, "title": "Order placed"}
+]
 
 # TASK: Create a route for "/"
 # This route should return a JSON welcome message
+@app.route("/")
+def home():
+    return jsonify(message='Welcome')
 
 # TASK: Create a GET route for "/events"
 # This route should return the full list of events as JSON
+@app.route("/events", methods = ["GET"])
+def get_events():
+    return jsonify(events)
 
 # TASK: Create a POST route for "/events"
 # This route should:
